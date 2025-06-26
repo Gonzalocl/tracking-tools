ef3c3c765a9e109633e04a14b9b1b8cf20aa5f28
Gonzalo Caparrós Laiz <gonzalocl1024@gmail.com>
2025-06-26 22:27:28 +0200

    readDayLabels

--- csv-to-group-by-date-kml/src/main/java/org/eu/gonzalocaparros/csv_to_group_by_date_kml/Main.java
+++ csv-to-group-by-date-kml/src/main/java/org/eu/gonzalocaparros/csv_to_group_by_date_kml/Main.java
@@ -1,5 +1,13 @@
 package org.eu.gonzalocaparros.csv_to_group_by_date_kml;
 
+import org.apache.commons.csv.CSVFormat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,5 +18,19 @@ public class Main {
             return;
         }
 
+        var inputDirectory = Path.of(args[0]);
+
+        var dayLabels = readDayLabels(inputDirectory);
+    }
+
+    private static Map<String, String> readDayLabels(Path inputDirectory) {
+
+        try (var reader = Files.newBufferedReader(inputDirectory.resolve("day_labels.csv"));
+             var stream = CSVFormat.DEFAULT.parse(reader).stream()) {
+
+            return stream.collect(Collectors.toMap(r -> r.get(0), r -> r.get(1)));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
